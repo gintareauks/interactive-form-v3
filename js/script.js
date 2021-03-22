eed36b9ec993b855dfde882d9b9f506df8af5fb0
Gintare Auks <gintareauks@gmail.com>
2021-03-22 11:30:19 +0200

    validation done

--- js/script.js
+++ js/script.js
@@ -63,19 +63,20 @@ const bitcoin = document.getElementById("bitcoin");
 
 paypal.style.display = "none";
 bitcoin.style.display = "none";
+creditCard.style.display = "block";
 
 payment.children[1].setAttribute('selected', true);
 
 payment.addEventListener('change', (event) => {
-    if (event.target.value == 'paypal') {
+    if (event.target.value === 'paypal') {
         paypal.style.display = "block";
         bitcoin.style.display = "none";
         creditCard.style.display = "none";
-    } else if (event.target.value == 'bitcoin') {
+    } else if (event.target.value === 'bitcoin') {
         paypal.style.display = "none";
         bitcoin.style.display = "block";
         creditCard.style.display = "none"
-    } else if (event.target.value == 'credit-card') {
+    } else if (event.target.value === 'credit-card') {
         paypal.style.display = "none";
         bitcoin.style.display = "none";
         creditCard.style.display = "block";
@@ -88,25 +89,64 @@ const cardNumber = document.getElementById("cc-num");
 const zipCode = document.getElementById("zip");
 const CVV = document.getElementById("cvv");
 const form = document.querySelector("form");
+const legendActivities = document.querySelector("#activities legend");
 
-console.log(form);
 
-function required(input){
-        event.preventDefault();
-
-    if (input.value.length == 0){
-        input.style.border = "2px solid red";
-        return false;
-    } else if(input.value.length !== 0){
-        input.style.borderColor = 'initial';
-        return true;
-    }
-    
+function inputEmpty(input){
+    return /^\s*$/.test(input);
 }
 
+function isValidEmail(input) {
+    return /^[^@]+@[^@.]+\.[a-z]+$/i.test(input);
+}
+
+function activitySelected(input) {
+    return /^[1-9]\d*$/.test(input);
+}
+
+function validCardNumber(input) {
+    return /^\d{13,16}$/.test(input);
+}
+
+function validZip(input) {
+    return /^\d{5}$/.test(input);
+}
+
+function validCVV(input) {
+    return /^\d{3}$/.test(input);
+}
 
 form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const testName = required(nameInput);
+    const nameValue = nameInput.value;
+    if (inputEmpty(nameValue) == true) {
+        nameInput.style.border = "2px solid red";
+        event.preventDefault();
+    } 
     
+    if (isValidEmail(email.value) == false) {
+        email.style.border = "2px solid red";
+        event.preventDefault();
+    }
+
+    if (activitySelected(total) == false) {
+        legendActivities.style.color = 'red';
+        event.preventDefault();
+    }
+
+    if (creditCard.style.display == 'block') {
+        if (validCardNumber(cardNumber.value) == false) {
+            cardNumber.style.border = "2px solid red";
+            event.preventDefault();
+        }
+
+        if (validZip(zipCode.value) == false) {
+            zipCode.style.border = "2px solid red";
+            event.preventDefault();
+        }
+
+        if (validCVV(CVV.value) == false) {
+            CVV.style.border = "2px solid red";
+            event.preventDefault();
+        }
+    }
 })
