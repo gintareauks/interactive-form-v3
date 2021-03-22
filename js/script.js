6e94ebe2bd20dd828c6bc585adacebbb00e7db6f
Gintare Auks <gintareauks@gmail.com>
2021-03-22 17:00:43 +0200

    exceeds, but still need to fix code

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
-// The "Name" Field
+// Name field is focused when page is just opened or refreshed;
 const nameInput = document.getElementById("name");
 nameInput.focus();
 
-// "Job Role" Section
+// Ability to type your own job only if 'other' is selected
 const otherJob = document.getElementById("other-job-role");
 otherJob.style.display = "none";
 
@@ -16,7 +16,7 @@ jobRole.addEventListener('change', (event) => {
 }
 })
 
-// "T-Shirt Info" Section
+// T-Shirt color available depends on the chosen design
 const color = document.getElementById("color");
 color.disabled = true;
 
@@ -38,7 +38,7 @@ design.addEventListener('change', (event) => {
     }
 })
 
-// "Register for Activities" section
+// Total cost of activities depends on which ones are chosen 
 const activities = document.getElementById("activities");
 const activitiesCost = document.getElementById("activities-cost");
 let total = 0;
@@ -55,7 +55,7 @@ activities.addEventListener('change', (event) => {
    activitiesCost.innerHTML = `Total: $${total}`;
 })
 
-// "Payment Info" section
+// Since only one payment option can be selected, info for non-selected options must then be hidden
 const payment = document.getElementById("payment");
 const creditCard = document.getElementById("credit-card");
 const paypal = document.getElementById("paypal");
@@ -83,7 +83,7 @@ payment.addEventListener('change', (event) => {
     }
 })
 
-// Form Validation
+// Form Validation and Accessibility applied
 const email = document.getElementById("email");
 const cardNumber = document.getElementById("cc-num");
 const zipCode = document.getElementById("zip");
@@ -97,7 +97,16 @@ function inputEmpty(input){
 }
 
 function isValidEmail(input) {
-    return /^[^@]+@[^@.]+\.[a-z]+$/i.test(input);
+    const emailHint = document.getElementById("email-hint")
+    const validEmail = /^[^@]+@[^@.]+\.[a-z]+$/i.test(input);
+
+    if (email.value === "") { 
+        emailHint.innerHTML = "Email field cannot be blank"
+    } else {
+        emailHint.innerHTML = "Email address must be formatted correctly";
+    }
+
+    return validEmail;
 }
 
 function activitySelected(input) {
@@ -106,6 +115,7 @@ function activitySelected(input) {
 
 function validCardNumber(input) {
     return /^\d{13,16}$/.test(input);
+     
 }
 
 function validZip(input) {
@@ -145,7 +155,9 @@ form.addEventListener('submit', (event) => {
         legendActivities.parentElement.lastElementChild.style.display = "block";
     }
 
+    
     if (creditCard.style.display == 'block') {
+
         if (validCardNumber(cardNumber.value) == false) {
             cardNumber.style.border = "2px solid red";
             event.preventDefault();
@@ -153,8 +165,8 @@ form.addEventListener('submit', (event) => {
             cardNumber.parentElement.classList.add("not-valid");
             cardNumber.parentElement.classList.remove("valid");
             cardNumber.parentElement.lastElementChild.style.display = "block";
-        }
-
+        } 
+  
         if (validZip(zipCode.value) == false) {
             zipCode.style.border = "2px solid red";
             event.preventDefault();
@@ -175,7 +187,7 @@ form.addEventListener('submit', (event) => {
     }
 })
 
-// Accessibility 
+// Activity option is focused when accessing it with tab
 const activityInputs = document.querySelectorAll('[type="checkbox"]')
 
 for (i = 0; i < activityInputs.length; i++) {
@@ -187,3 +199,53 @@ for (i = 0; i < activityInputs.length; i++) {
         event.target.parentElement.className = "blur";
     })
 }
+
+// EXCEEDS
+
+activities.addEventListener('click', (event) => {    
+    const eventAttribute = event.target.getAttribute('data-day-and-time');
+
+    for (i = 0; i < activityInputs.length; i++) {
+    const compare = activityInputs[i].getAttribute('data-day-and-time');
+
+    if (eventAttribute == compare && event.target !== activityInputs[i]) {
+        activityInputs[i].disabled = true;
+
+        if (event.target.checked == true) {
+        activityInputs[i].parentElement.classList.add("disabled");
+        activityInputs[i].disabled = true;
+        } else {
+        activityInputs[i].parentElement.classList.remove("disabled");
+        activityInputs[i].disabled = false;
+        }
+    }
+    }   
+})
+
+
+creditCard.addEventListener('keyup', () => {
+    if (validCardNumber(cardNumber.value) == false) {
+        cardNumber.style.border = "2px solid red";
+        event.preventDefault();
+
+        cardNumber.parentElement.classList.add("not-valid");
+        cardNumber.parentElement.classList.remove("valid");
+        cardNumber.parentElement.lastElementChild.style.display = "block";
+    } else {
+            cardNumber.style.border = "initial";
+            cardNumber.parentElement.classList.add("valid");
+            cardNumber.parentElement.classList.remove("not-valid");
+            cardNumber.parentElement.lastElementChild.style.display = "none";
+        }
+})
+
+
+
+
+
+
+
+
+
+
+
